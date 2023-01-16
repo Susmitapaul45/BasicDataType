6a9c218cd79ce81c3f4acb873a7ece05912aade7
susmita19045 <susmitapaul22123@gmail.com>
2023-01-16 23:24:28 +0600

    first file

---
+++ basic data type 1.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a>>b>>c>>d>>e;
+   cout<< *&a,*&b,*&c,*&d,*&e;
+    return 0;
+
+}
